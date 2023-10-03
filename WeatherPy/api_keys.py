cb1960776df2635187d35537c47fc9b603f5dd8c
iHashmi9 <ibsar_hashmi@hotmail.com>
2023-10-02 22:54:14 -0400

    third commit

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "efb907f71a6a6beb4d4ce2909c6f0a4f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "f25e0b7f404546b586ff186c08079b78"
